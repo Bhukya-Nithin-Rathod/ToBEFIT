750f1caccbd6989599e9e1e93cb4f501d96f4647
Nithin <105960383+Bhukya-Nithin-Rathod@users.noreply.github.com>
2024-07-21 18:10:58 +0530

    Add files via upload

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '25fed2025amshb86974a92883253p139533jsn543b471898a0',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
